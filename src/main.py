d1f06465fc463bcf416ef8e7af1475d4ac350d74
Luís Sousa Rêgo <luis.sousa.rego@gmail.com>
2019-03-10 20:05:28 +0000

    get text and process it

--- src/main.py
+++ src/main.py
@@ -1,8 +1,25 @@
 import praw
+import re
 
-user_agent = "linux:imitante:v0.0.1 (by /u/imitante)"
 
-reddit = praw.Reddit('imitante', user_agent=user_agent)
+def getPortugal():
+    user_agent = "linux:imitante:v0.0.1 (by /u/imitante)"
+    reddit = praw.Reddit('imitante', user_agent=user_agent)
+    return reddit.subreddit('portugal')
 
-for submission in reddit.subreddit('learnpython').hot(limit=10):
-    print(submission.title)
+def removeLinks(text):
+    regex = r"\[(.+)]\(http.+\)"
+    subst = "\\1"
+    result = re.sub(regex, subst, text, 0, re.IGNORECASE)
+    return result
+
+portugal = getPortugal()
+palavras = set()
+
+for submission in portugal.hot(limit=10):
+    if submission.is_self:
+        palavras = {post for post in removeLinks(submission.selftext).split()}
+
+print(palavras)
+
+
